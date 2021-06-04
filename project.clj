4c2a11281154cb1d04dc941cae4a0cd480a82452
Axel Schüssler <axel@axels.cloud>
2021-06-04 10:49:57 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/bitterblue/commonmark-hiccup"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.atlassian.commonmark/commonmark "0.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.atlassian.commonmark/commonmark "0.17.0"]
                  [hiccup "1.0.5"]])
