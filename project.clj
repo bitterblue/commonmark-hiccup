5703e0d38f37e84aeb6af39423e53e6a7631cf21
Axel Schüssler <axel@axels.cloud>
2021-06-04 11:19:48 +0200

    Update deployment configuration for clojars

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.atlassian.commonmark/commonmark "0.17.0"]
-                 [hiccup "1.0.5"]])
+                 [hiccup "1.0.5"]]
+  :repositories [["releases" {:url "https://repo.clojars.org"
+                              :creds :gpg}]])
+
